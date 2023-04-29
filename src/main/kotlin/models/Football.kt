669283541a120f3911e228cd2a6c4166f840b514
joelamoakoh <20096482@mail.wit.ie>
2023-04-29 09:06:47 +0100

    new model added called Football.

---
+++ src/main/kotlin/models/Football.kt
@@ -0,0 +1,7 @@
+package models
+
+data class Football(val teamName: String,
+                val teamPriority: Int,
+                val League: String,
+                val isNoteArchived :Boolean){
+}
