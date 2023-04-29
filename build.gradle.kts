557d62884c59635fa522f056214668fba7c2ac4d
joelamoakoh <20096482@mail.wit.ie>
2023-04-29 09:55:58 +0100

    JUnit tests added.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ repositories {
 dependencies {
     testImplementation("org.slf4j:slf4j-simple:2.0.6")
     implementation("io.github.microutils:kotlin-logging:3.0.4")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("junit:junit:4.13.1")
 }
 
 tasks.test {
