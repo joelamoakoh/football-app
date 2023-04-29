5457771e0d01fb323350f90bb3a358d4adf59fef
joelamoakoh <20096482@mail.wit.ie>
2023-04-29 08:25:42 +0100

    first commit (failed)

---
+++ src/main/kotlin/utils/ScannerInput.kt
@@ -0,0 +1,50 @@
+/**
+ Joel Amoakoh
+ 1.0
+*/
+
+package utils
+
+import java.lang.NumberFormatException
+import java.util.Scanner
+
+
+
+object ScannerInput {
+
+    @JvmStatic
+    fun readNextInt(prompt: String?): Int {
+        do {
+            try {
+                print(prompt)
+                return Scanner(System.`in`).next().toInt()
+            } catch (e: NumberFormatException) {
+                System.err.println("\tEnter a number please.")
+            }
+        } while (true)
+    }
+
+
+    fun readNextDouble(prompt: String?): Double {
+        do {
+            try {
+                print(prompt)
+                return Scanner(System.`in`).next().toDouble()
+            } catch (e: NumberFormatException) {
+                System.err.println("\tEnter a number please.")
+            }
+        } while (true)
+    }
+
+    @JvmStatic
+    fun readNextLine(prompt: String?): String {
+        print(prompt)
+        return Scanner(System.`in`).nextLine()
+    }
+
+    @JvmStatic
+    fun readNextChar(prompt: String?): Char {
+        print(prompt)
+        return Scanner(System.`in`).next()[0]
+    }
+}
